521eb752dd42f04f005d01eaccec85512fb536f6
JeremieUkundwa <JeremieUkundwa@github.com>
2023-12-24 17:46:39 +0200

    project creation

---
+++ src/main/java/account/mgt/useraccountmanagment/UserAccountManagmentApplication.java
@@ -0,0 +1,13 @@
+package account.mgt.useraccountmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAccountManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAccountManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/account/mgt/useraccountmanagment/UserAccountManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package account.mgt.useraccountmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAccountManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
